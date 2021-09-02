0717927d27eaf6519a8e3a20f253a397c23b064f
SreelekhaAshokKumar <sreelekhashokumar@gmail.com>
2021-09-02 15:19:55 +0200

    added files

---
+++ src/test/java/com/example/springrestserviceinitial/SpringRestServiceInitialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springrestserviceinitial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestServiceInitialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
